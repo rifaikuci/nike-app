4a2956bcf8e7d27782cbe2927a091d7086ad2074
Rifai Kuçi <rifai.kuci@finastech.com>
2023-08-22 15:37:04 +0300

    part 2

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -1,9 +1,34 @@
+import PopularProductCard from "../components/PopularProductCard.jsx";
+import {products} from "../constants/index.js";
 
 const PopularProduct = () => {
     return (
-        <div>
-            PopularProduct
-        </div>
+        <section
+            id={"products"}
+            className={"max-container max-sm:mt-12"}
+        >
+            <div className={"flex flex-col  justify-start gap-5"}>
+                <h2 className={"text-4xl font-palanquin font-bold"}>
+                    <span>
+                        <span className={"text-coral-red"}>
+                            Popular
+                        </span> Products
+                    </span>
+                </h2>
+                <p className={"lg:max-w-lg mt-2 font-montserrat text-slate-gray"}>
+                    Experience top-notch quality and style with our sought-after selections. Discover a world of comfor, design, and value
+                </p>
+                <div className={"mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14"}>
+                    {
+                        products.map((product) => (
+                            <PopularProductCard key={product.name} {...product} />
+                        ))
+                    }
+                </div>
+            </div>
+
+
+        </section>
     )
 }
 
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,17 @@
+import ServiceCard from "../components/ServiceCard.jsx";
+import {services} from "../constants/index.js";
 
 const Services = () => {
     return (
-        <div>
-            Services
-        </div>
+        <section className={"max-container flex justify-center flex-wrap gap-9"}>
+
+            {
+                services.map((service) => (
+                    <ServiceCard key={service.label} {...service} />
+                ) )
+            }
+
+        </section>
     )
 }
 
